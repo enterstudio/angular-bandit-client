9fd5e7262ade0b3b28b0afa5727c29551b1ebe6c
Jose Peleteiro <jose@peleteiro.net>
2015-01-09 12:11:05 -0200

    Added test:watch to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var testingSrc = [
   'node_modules/angular-mocks/angular-mocks.js',
   'node_modules/angular-localforage/dist/angular-localForage.js',
   "src/*.js",
+  "test/setup.coffee",
   "test/*Spec.coffee"
 ]
 
@@ -22,4 +23,13 @@ gulp.task('test', function() {
     .on('error', function(err) {
       throw err; // Make sure failed tests cause gulp to exit non-zero
     });
+});
+
+gulp.task('test:watch', function() {
+  return gulp
+    .src(testingSrc)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }))
 });
